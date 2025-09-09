1d20c67f0529c254b9b4671ccb318b495df43b21
elb <s214680@dtu.dk>
2025-09-09 12:52:03 +0200

    LED.h and LED.c

---
+++ project2/Inc/led.h
@@ -0,0 +1,12 @@
+#ifndef LED_H_
+#define LED_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f30x_conf.h"
+#include <stdio.h>
+
+/* Exported functions ------------------------------------------------------- */
+void initLED();
+void setLED(char sel);
+
+#endif /* JOYSTICK_H_ */
