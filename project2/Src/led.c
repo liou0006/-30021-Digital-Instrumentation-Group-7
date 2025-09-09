f5c676d8e65b76db41071ef794618f29110a7724
elb <s214680@dtu.dk>
2025-09-09 13:00:58 +0200

    Updated LED.c to include the main.c code

--- project2/Src/led.c
+++ project2/Src/led.c
@@ -1,5 +1,7 @@
 #include "led.h"
 
+// Code needed in main.c is included in the bottom of the file
+
 void initLED() {
 	// Enable clock for GPIO Ports
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);	// Port A
@@ -87,3 +89,48 @@ void setLED(char sel) {
 	}
 }
 
+
+
+// main.c code
+/*
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h" // Input/output library for this course
+
+#include "joystick.h"
+#include "led.h"
+
+int main(void) {
+	uart_init( 9600 ); // Initialize USB serial at 9600 baud
+
+	initJoystick();
+	initLED();
+
+	int8_t prev_state = readJoystick();
+	int8_t current_state;
+
+	while(1) {
+		current_state = readJoystick();
+		if (prev_state != current_state) {
+			if (current_state == 1) {
+				setLED('r');
+				printf("UP (red)\n");
+			} else if (current_state == 2) {
+				setLED('g');
+				printf("DOWN (green)\n");
+			} else if (current_state == 4) {
+				setLED('b');
+				printf("LEFT (blue)\n");
+			} else if (current_state == 8) {
+				setLED('c');
+				printf("RIGHT (cyan)\n");
+			} else if (current_state == 16) {
+				setLED('m');
+				printf("CENTER (magenta)\n");
+			} else {
+				setLED('d');
+			}
+			prev_state = current_state;
+		}
+	}
+}
+*/
