8791238190789c135fae7f706193f79edd8b8197
elb <97177091+s214680@users.noreply.github.com>
2025-09-16 12:13:34 +0200

    Create window.c

---
+++ project2/Src/window.c
@@ -0,0 +1,34 @@
+#include "window.h"
+
+void clrscr(void) {
+	/* ANSI escape sequence:
+	 * 	\033[2J clears screen
+	 * 	\033[H moves cursor to top left
+	 */
+	printf("\033[2J\033[H");
+	fflush(stdout);
+}
+
+void initStopWatchWindow() {
+	clrscr();
+
+	printf("\033[37;40m");	// set terminal to white text on black background
+
+	// Print title bar of width 30
+	printTitleBar("Stop watch", 30);
+}
+
+void printTitleBar(const char *title, int width) {
+	int padding = width - strlen(title);
+
+	// Title with inverted colours
+	printf("\033[7m %s", title);
+
+	// Fill rest with spaces
+	for (int i = 0; i < padding - 1; i++) {
+		printf(" ");
+	}
+
+	// Reset the color (white text on black background)
+	printf("\033[0m\n");
+}
